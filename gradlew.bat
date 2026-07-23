5fa6edd0b2873b582412d30187ee97867ef3d841
littlhMW <littlh@users.noreply.github.com>
2026-07-24 01:04:32 +0800

    chore: add Gradle wrapper files

---
+++ gradlew.bat
@@ -0,0 +1,18 @@
+@rem
+@rem Gradle start up script for Windows
+@rem
+@if "%DEBUG%"=="" @echo off
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+@rem This is normally unused
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+java %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
